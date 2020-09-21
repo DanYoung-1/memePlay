b0a098646c443dd1f083965deee3c480287d9034
Daniel Young <Daniel Young>
2020-09-21 17:31:53 -0600

    Initial Commit

---
+++ memePlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memePlay
+//
+//  Created by Daniel Young on 9/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memePlay/memePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  memePlayApp.swift
+//  memePlay
+//
+//  Created by Daniel Young on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct memePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
